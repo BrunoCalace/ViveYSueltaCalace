b1893447eba8b1724e4d3c2a23dea23740f04b43
Bruno Calace <bruno.calace@gmail.com>
2023-07-06 12:53:17 -0300

    Utilizando fetch/FUNCA

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,20 +1,3 @@
-const productos = [
-    {id:1, nom: "bolso negro", precio: 7500, img:'Productos/bolso1.jpg', cant: 1, cat: "bolsos"},
-    {id:2, nom: "bolso marron", precio: 8500, img:'Productos/bolso2.jpg', cant: 1, cat: "bolsos"},
-    {id:3, nom: "cartera cuero", precio: 6500, img:'Productos/cartera1.jpg', cant: 1, cat: "carteras"},
-    {id:4, nom: "cartera colores", precio: 10000, img:'Productos/cartera2.jpg', cant: 1, cat: "carteras"},
-    {id:5, nom: "cartera azul", precio: 12000, img:'Productos/cartera3.jpg', cant: 1, cat: "carteras"},
-    {id:6, nom: "cinto marron", precio: 2500, img:'Productos/cinto2.jpg', cant: 1, cat: "cintos"},
-    {id:7, nom: "cinto negro", precio: 2500, img:'Productos/cinto3.jpg', cant: 1, cat: "cintos"},
-    {id:8, nom: "cinturon caballo", precio: 3000, img:'Productos/cinto4.jpg', cant: 1, cat: "cintos"},
-    {id:9, nom: "mochila mediana", precio: 13500, img:'Productos/mochila2.jpg', cant: 1, cat: "mochilas"},
-    {id:10, nom: "mochila purpura", precio: 14500, img:'Productos/mochila3.jpg', cant: 1, cat: "mochilas"},
-    {id:11, nom: "panchas beige", precio: 16500, img:'Productos/panchas1.jpg', cant: 1, cat: "calzado"},
-    {id:12, nom: "panchas negras", precio: 15500, img:'Productos/panchas2.jpg', cant: 1, cat: "calzado"},
-    {id:13, nom: "sandalias rojas", precio: 12500, img:'Productos/sandalia5.jpg', cant: 1, cat: "calzado"},
-    {id:14, nom: "sandalias negras", precio: 12500, img:'Productos/sandalias2.jpg', cant: 1, cat: "calzado"},
-    {id:15, nom: "sandalias marrones", precio: 12500, img:'Productos/sandalia7.jpg', cant: 1, cat: "calzado"},
-]
 const shopContent = document.getElementById("shopContent")
 const verCarrito = document.getElementById("verCarrito")
 const modalContainer = document.getElementById("modal-container")
@@ -22,46 +5,51 @@ const cantCarrito = document.getElementById("cantCarrito")
 
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-productos.forEach((product) =>{
-    let content = document.createElement("div");
-    content.className = "card";
-    let cat = product.cat;
-    content.classList.add(cat);
-    content.classList.add("hide");
-    content.innerHTML = `
-        <img class="card-img" src="${product.img}">
-        <h3 class="card-nom">${product.nom}</h3>
-        <p class="card-price">$ ${product.precio}</p>
-    `;
-
-    shopContent.append(content);
-
-    let comprar = document.createElement("button");
-    comprar.innerText= "Agregar al carrito";
-    comprar.className = "card-btn";
-
-    content.append(comprar);
+fetch('data.json')
+.then( (res) => res.json())
+.then( (data) => {
     
-    comprar.addEventListener("click", () =>{
-
-        const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
+    data.forEach((product) =>{
+        let content = document.createElement("div");
+        content.className = "card";
+        let cat = product.cat;
+        content.classList.add(cat);
+        content.innerHTML = `
+            <img class="card-img" src="${product.img}">
+            <h3 class="card-nom">${product.nom}</h3>
+            <p class="card-price">$ ${product.precio}</p>
+        `;
+    
+        shopContent.append(content);
+    
+        let comprar = document.createElement("button");
+        comprar.innerText= "Agregar al carrito";
+        comprar.className = "card-btn";
+    
+        content.append(comprar);
         
-        if(repeat){
+        comprar.addEventListener("click", () =>{
+    
+            const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
             
-        }else {
-            carrito.push({
-                id: product.id,
-                img: product.img,
-                nom: product.nom,
-                precio: product.precio,
-                cant: product.cant,
-            });
-        }
-        console.log(carrito);
-        carritoCounter();
-        saveLocal();
+            if(repeat){
+                
+            }else {
+                carrito.push({
+                    id: product.id,
+                    img: product.img,
+                    nom: product.nom,
+                    precio: product.precio,
+                    cant: product.cant,
+                });
+            }
+            console.log(carrito);
+            carritoCounter();
+            saveLocal();
+        });
     });
-});
+})
+
 
 const pintarCarrito = () => {
     modalContainer.innerHTML = "";
